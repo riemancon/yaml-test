5128e16a4176e585332e707c33682c7735029471
steve <steve@imac0.local>
2011-07-15 16:18:12 -0400

    Initial Commit

---
+++ yaml-test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  yaml-test
+//
+//  Created by steve on 7/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
